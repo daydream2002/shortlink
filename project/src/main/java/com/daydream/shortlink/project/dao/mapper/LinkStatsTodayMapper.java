b8d5b8bc4a4f9bc60904d0ed62e6576246525fd2
daydream <daydream_wjh@qq.com>
2025-02-08 16:36:36 +0800

    feat(project): 添加今日短链接统计数据模型及相关字段

---
+++ project/src/main/java/com/daydream/shortlink/project/dao/mapper/LinkStatsTodayMapper.java
@@ -0,0 +1,12 @@
+package com.daydream.shortlink.project.dao.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.daydream.shortlink.project.dao.entity.LinkStatsTodayDO;
+
+/**
+ * Author daydream
+ * Description
+ * Date 2025/2/8 16:35
+ */
+public interface LinkStatsTodayMapper extends BaseMapper<LinkStatsTodayDO> {
+}
