7c0a250a491ba55d2343f24d56b8b4020f0c382b
daydream <daydream_wjh@qq.com>
2025-02-08 16:54:40 +0800

    feat(project): 添加短链接访问统计自增功能

--- project/src/main/java/com/daydream/shortlink/project/dao/mapper/ShortLinkMapper.java
+++ project/src/main/java/com/daydream/shortlink/project/dao/mapper/ShortLinkMapper.java
@@ -2,6 +2,8 @@ package com.daydream.shortlink.project.dao.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.daydream.shortlink.project.dao.entity.ShortLinkDO;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Update;
 
 /**
  * Author daydream
@@ -9,4 +11,15 @@ import com.daydream.shortlink.project.dao.entity.ShortLinkDO;
  * Date 2025/1/10 16:13
  */
 public interface ShortLinkMapper extends BaseMapper<ShortLinkDO> {
+    /**
+     * 短链接访问统计自增
+     */
+    @Update("update t_link set total_pv = total_pv + #{totalPv}, total_uv = total_uv + #{totalUv}, total_uip = total_uip + #{totalUip} where gid = #{gid} and full_short_url = #{fullShortUrl}")
+    void incrementStats(
+            @Param("gid") String gid,
+            @Param("fullShortUrl") String fullShortUrl,
+            @Param("totalPv") Integer totalPv,
+            @Param("totalUv") Integer totalUv,
+            @Param("totalUip") Integer totalUip
+    );
 }
