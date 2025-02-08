d1bca2c93faff718655d338d1cde31408a85efda
daydream <daydream_wjh@qq.com>
2025-02-08 10:57:49 +0800

    refactor(project): 优化短链接访问统计接口返回参数
    -移除了不必要的导入
    - 将 Date 类型的 date 字段改为 String 类型
    - 删除了 @JsonFormat 注解

--- project/src/main/java/com/daydream/shortlink/project/dto/resp/ShortLinkStatsAccessDailyRespDTO.java
+++ project/src/main/java/com/daydream/shortlink/project/dto/resp/ShortLinkStatsAccessDailyRespDTO.java
@@ -6,13 +6,10 @@ package com.daydream.shortlink.project.dto.resp;
  * Date 2025/2/7 17:11
  */
 
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.Date;
 
 /**
  * 短链接基础访问监控响应参数
@@ -26,8 +23,7 @@ public class ShortLinkStatsAccessDailyRespDTO {
     /**
      * 日期
      */
-    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
-    private Date date;
+    private String date;
 
     /**
      * 访问量
