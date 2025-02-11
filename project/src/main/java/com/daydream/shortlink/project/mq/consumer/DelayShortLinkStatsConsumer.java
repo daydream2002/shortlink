8e4b267c7ddd4e6a9e02f9067f661dce7674e5d4
daydream <daydream_wjh@qq.com>
2025-02-11 21:58:33 +0800

    feat(project): 添加消息队列唯一标识及幂等性处理功能

--- project/src/main/java/com/daydream/shortlink/project/mq/consumer/DelayShortLinkStatsConsumer.java
+++ project/src/main/java/com/daydream/shortlink/project/mq/consumer/DelayShortLinkStatsConsumer.java
@@ -1,8 +1,11 @@
 package com.daydream.shortlink.project.mq.consumer;
 
+import com.daydream.shortlink.project.common.convention.exception.ServiceException;
 import com.daydream.shortlink.project.dto.biz.ShortLinkStatsRecordDTO;
+import com.daydream.shortlink.project.mq.idempotent.MessageQueueIdempotentHandler;
 import com.daydream.shortlink.project.service.ShortLinkService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RBlockingDeque;
 import org.redisson.api.RDelayedQueue;
 import org.redisson.api.RedissonClient;
@@ -16,10 +19,12 @@ import static com.daydream.shortlink.project.common.constant.RedisKeyConstant.DE
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class DelayShortLinkStatsConsumer implements InitializingBean {
 
     private final RedissonClient redissonClient;
     private final ShortLinkService shortLinkService;
+    private final MessageQueueIdempotentHandler messageQueueIdempotentHandler;
 
     public void onMessage() {
         Executors.newSingleThreadExecutor(
@@ -36,7 +41,20 @@ public class DelayShortLinkStatsConsumer implements InitializingBean {
                         try {
                             ShortLinkStatsRecordDTO statsRecord = delayedQueue.poll();
                             if (statsRecord != null) {
-                                shortLinkService.shortLinkStats(null, null, statsRecord);
+                                if (!messageQueueIdempotentHandler.isMessageProcessed(statsRecord.getKeys())) {
+                                    // 判断当前的这个消息流程是否执行完成
+                                    if (messageQueueIdempotentHandler.isAccomplish(statsRecord.getKeys())) {
+                                        return;
+                                    }
+                                    throw new ServiceException("消息未完成流程，需要消息队列重试");
+                                }
+                                try {
+                                    shortLinkService.shortLinkStats(null, null, statsRecord);
+                                } catch (Throwable ex) {
+                                    messageQueueIdempotentHandler.delMessageProcessed(statsRecord.getKeys());
+                                    log.error("延迟记录短链接监控消费异常", ex);
+                                }
+                                messageQueueIdempotentHandler.setAccomplish(statsRecord.getKeys());
                                 continue;
                             }
                             LockSupport.parkUntil(500);
@@ -51,4 +69,3 @@ public class DelayShortLinkStatsConsumer implements InitializingBean {
         onMessage();
     }
 }
-
--- project/src/main/java/com/daydream/shortlink/project/mq/producer/DelayShortLinkStatsProducer.java
+++ project/src/main/java/com/daydream/shortlink/project/mq/producer/DelayShortLinkStatsProducer.java
@@ -1,5 +1,6 @@
 package com.daydream.shortlink.project.mq.producer;
 
+import cn.hutool.core.lang.UUID;
 import com.daydream.shortlink.project.dto.biz.ShortLinkStatsRecordDTO;
 import lombok.RequiredArgsConstructor;
 import org.redisson.api.RBlockingDeque;
@@ -23,6 +24,7 @@ public class DelayShortLinkStatsProducer {
      * @param statsRecord 短链接统计实体参数
      */
     public void send(ShortLinkStatsRecordDTO statsRecord) {
+        statsRecord.setKeys(UUID.fastUUID().toString());
         RBlockingDeque<ShortLinkStatsRecordDTO> blockingDeque = redissonClient.getBlockingDeque(DELAY_QUEUE_STATS_KEY);
         RDelayedQueue<ShortLinkStatsRecordDTO> delayedQueue = redissonClient.getDelayedQueue(blockingDeque);
         delayedQueue.offer(statsRecord, 5, TimeUnit.SECONDS);
